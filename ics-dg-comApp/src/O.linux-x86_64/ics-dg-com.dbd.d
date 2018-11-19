fd07281ef115b509998ce6aa7bc98563bd753ec8
iocuser <iocuser@localhost.localdomain>
2018-11-19 15:28:26 +0000

    Initial Commit

---
+++ ics-dg-comApp/src/O.linux-x86_64/ics-dg-com.dbd.d
@@ -0,0 +1,106 @@
+../O.Common/ics-dg-com.dbd: /epics/base/dbd/base.dbd \
+    /epics/base/dbd/menuGlobal.dbd \
+    /epics/base/dbd/menuAlarmSevr.dbd \
+    /epics/base/dbd/menuAlarmStat.dbd \
+    /epics/base/dbd/menuFtype.dbd \
+    /epics/base/dbd/menuIvoa.dbd \
+    /epics/base/dbd/menuOmsl.dbd \
+    /epics/base/dbd/menuPini.dbd \
+    /epics/base/dbd/menuPost.dbd \
+    /epics/base/dbd/menuPriority.dbd \
+    /epics/base/dbd/menuYesNo.dbd \
+    /epics/base/dbd/menuSimm.dbd \
+    /epics/base/dbd/menuConvert.dbd \
+    /epics/base/dbd/menuScan.dbd \
+    /epics/base/dbd/stdRecords.dbd \
+    /epics/base/dbd/aaiRecord.dbd \
+    /epics/base/dbd/dbCommon.dbd \
+    /epics/base/dbd/aaoRecord.dbd \
+    /epics/base/dbd/aiRecord.dbd \
+    /epics/base/dbd/aoRecord.dbd \
+    /epics/base/dbd/aSubRecord.dbd \
+    /epics/base/dbd/biRecord.dbd \
+    /epics/base/dbd/boRecord.dbd \
+    /epics/base/dbd/calcRecord.dbd \
+    /epics/base/dbd/calcoutRecord.dbd \
+    /epics/base/dbd/compressRecord.dbd \
+    /epics/base/dbd/dfanoutRecord.dbd \
+    /epics/base/dbd/eventRecord.dbd \
+    /epics/base/dbd/fanoutRecord.dbd \
+    /epics/base/dbd/histogramRecord.dbd \
+    /epics/base/dbd/longinRecord.dbd \
+    /epics/base/dbd/longoutRecord.dbd \
+    /epics/base/dbd/lsiRecord.dbd \
+    /epics/base/dbd/lsoRecord.dbd \
+    /epics/base/dbd/mbbiRecord.dbd \
+    /epics/base/dbd/mbbiDirectRecord.dbd \
+    /epics/base/dbd/mbboRecord.dbd \
+    /epics/base/dbd/mbboDirectRecord.dbd \
+    /epics/base/dbd/permissiveRecord.dbd \
+    /epics/base/dbd/printfRecord.dbd \
+    /epics/base/dbd/selRecord.dbd \
+    /epics/base/dbd/seqRecord.dbd \
+    /epics/base/dbd/stateRecord.dbd \
+    /epics/base/dbd/stringinRecord.dbd \
+    /epics/base/dbd/stringoutRecord.dbd \
+    /epics/base/dbd/subRecord.dbd \
+    /epics/base/dbd/subArrayRecord.dbd \
+    /epics/base/dbd/waveformRecord.dbd \
+    /epics/base/dbd/filters.dbd \
+    /epics/base/dbd/devSoft.dbd \
+    /epics/base/dbd/asSub.dbd \
+    /epics/base/dbd/dbCore.dbd
+
+/epics/base/dbd/base.dbd:
+/epics/base/dbd/menuGlobal.dbd:
+/epics/base/dbd/menuAlarmSevr.dbd:
+/epics/base/dbd/menuAlarmStat.dbd:
+/epics/base/dbd/menuFtype.dbd:
+/epics/base/dbd/menuIvoa.dbd:
+/epics/base/dbd/menuOmsl.dbd:
+/epics/base/dbd/menuPini.dbd:
+/epics/base/dbd/menuPost.dbd:
+/epics/base/dbd/menuPriority.dbd:
+/epics/base/dbd/menuYesNo.dbd:
+/epics/base/dbd/menuSimm.dbd:
+/epics/base/dbd/menuConvert.dbd:
+/epics/base/dbd/menuScan.dbd:
+/epics/base/dbd/stdRecords.dbd:
+/epics/base/dbd/aaiRecord.dbd:
+/epics/base/dbd/dbCommon.dbd:
+/epics/base/dbd/aaoRecord.dbd:
+/epics/base/dbd/aiRecord.dbd:
+/epics/base/dbd/aoRecord.dbd:
+/epics/base/dbd/aSubRecord.dbd:
+/epics/base/dbd/biRecord.dbd:
+/epics/base/dbd/boRecord.dbd:
+/epics/base/dbd/calcRecord.dbd:
+/epics/base/dbd/calcoutRecord.dbd:
+/epics/base/dbd/compressRecord.dbd:
+/epics/base/dbd/dfanoutRecord.dbd:
+/epics/base/dbd/eventRecord.dbd:
+/epics/base/dbd/fanoutRecord.dbd:
+/epics/base/dbd/histogramRecord.dbd:
+/epics/base/dbd/longinRecord.dbd:
+/epics/base/dbd/longoutRecord.dbd:
+/epics/base/dbd/lsiRecord.dbd:
+/epics/base/dbd/lsoRecord.dbd:
+/epics/base/dbd/mbbiRecord.dbd:
+/epics/base/dbd/mbbiDirectRecord.dbd:
+/epics/base/dbd/mbboRecord.dbd:
+/epics/base/dbd/mbboDirectRecord.dbd:
+/epics/base/dbd/permissiveRecord.dbd:
+/epics/base/dbd/printfRecord.dbd:
+/epics/base/dbd/selRecord.dbd:
+/epics/base/dbd/seqRecord.dbd:
+/epics/base/dbd/stateRecord.dbd:
+/epics/base/dbd/stringinRecord.dbd:
+/epics/base/dbd/stringoutRecord.dbd:
+/epics/base/dbd/subRecord.dbd:
+/epics/base/dbd/subArrayRecord.dbd:
+/epics/base/dbd/waveformRecord.dbd:
+/epics/base/dbd/filters.dbd:
+/epics/base/dbd/devSoft.dbd:
+/epics/base/dbd/asSub.dbd:
+/epics/base/dbd/dbCore.dbd:
+../O.Common/ics-dg-com.dbd: ../Makefile
